fc935e6bd836221e74b4a55b79faff03c9110ace
MarcoANT9 <acalde27@hotmail.com>
2020-12-07 22:48:32 +0000

    Task 0: Pep8 Revision & Changed to exe

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 
+
 def checkKeys(key, boxes, open_boxes):
     """This is the function to use recursion."""
     for element in boxes[key]:
-    
-        if (element < len(boxes) and open_boxes[element] == False and boxes[key] != []):
+
+        if (element < len(boxes) and open_boxes[element] is False and
+                boxes[key] != []):
             open_boxes[element] = True
             open_boxes = checkKeys(element, boxes, open_boxes)
         else:
@@ -14,17 +16,18 @@ def checkKeys(key, boxes, open_boxes):
 
 
 def canUnlockAll(boxes):
-    """This function returns True if all the boxes can be unlocked and False if they cannon."""
+    """This function returns True if all the boxes can be unlocked and False if
+    they cannon."""
     lis_en = len(boxes)
 
-    if boxes == None or lis_en == 0:
+    if (boxes is None or lis_en == 0):
         return False
 
     open_boxes = [False] * lis_en
     open_boxes[0] = True
 
     for key in boxes[0]:
-        if (key < lis_en and open_boxes[key] == False):
+        if (key < lis_en and open_boxes[key] is False):
             open_boxes[key] = True
             open_boxes = checkKeys(key, boxes, open_boxes)
         else:

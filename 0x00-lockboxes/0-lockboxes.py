8161e47fe5f1d0b84d1fd92d2306e70d54dcb5a8
MarcoAnt9 <acalde27@hotmail.com>
2020-12-07 17:34:40 -0500

    Task 0: First Commit

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,29 @@
+def checkKeys(key, boxes, open_boxes):
+    for element in boxes[key]:
+        if (element < len(boxes) and open_boxes[element] == False and boxes[key] != []):
+            open_boxes[element] = True
+            open_boxes = checkKeys(element, boxes, open_boxes)
+        else:
+            break
+    return open_boxes
+
+
+def canUnlockAll(boxes):
+    lis_en = len(boxes)
+
+    if boxes == None or lis_en == 0:
+        return False
+
+    open_boxes = [False] * lis_en
+    open_boxes[0] = True
+
+    for key in boxes[0]:
+        if (key < lis_en and open_boxes[key] == False):
+            open_boxes[key] = True
+            open_boxes = checkKeys(key, boxes, open_boxes)
+        else:
+            continue
+
+    if (False in open_boxes):
+        return (False)
+    return (True)

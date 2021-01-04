2294364bddcd19e018ac752748d958d7bdadd18a
MarcoANT9 <acalde27@hotmail.com>
2021-01-04 22:00:37 +0000

    Task 0: First Commit

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Main file for the operation counting.
+"""
+
+def minOperations(n):
+    """This program uses the module operation to determine when to copy the
+    string, if the module between the number n and the lenght of the current
+    output will be copied and pasted, if the module is not zero it will only
+    paste."""
+
+    count = 0
+    copy = "h"
+    if (n < 2):
+        return count
+
+
+    string = "h"
+    while (len(string) < n):
+        if (n % len(string) == 0):
+            copy = string
+            count += 1
+        string = string + copy
+        count += 1
+
+    return count

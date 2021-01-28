c74b1d6917ff82b6e13f9cf082591dcf4d7bdfdc
MarcoAnt9 <acalde27@hotmail.com>
2021-01-28 15:01:15 -0500

    Create 0-stats.py

---
+++ 0x06-log_parsing/0-stats.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""This module reads the stdin and prints the metrics."""
+import sys
+
+if __name__ == "__main__":
+    status = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+              "404": 0, "405": 0, "500": 0}
+    size = 0
+    print10Lines = 0
+
+    try:
+
+        for text in sys.stdin:
+            code = text.split('"')[2].split(" ")[1]
+            unitSize = int(text.split('"')[2].split(" ")[2])
+            size += unitSize
+            print10Lines += 1
+
+            for key in sorted(status.keys()):
+                if code == key:
+                    status[key] += 1
+            if print10Lines == 10:
+                print("File size: {:d}".format(size))
+                for key in sorted(status.keys()):
+                    if status[key]:
+                        print("{}: {:d}".format(key, status[key]))
+                print10Lines = 0
+
+    except KeyboardInterrupt:
+        pass
+
+    finally:
+        print("File size: {:d}".format(size))
+
+        for key in sorted(status.keys()):
+            if status[key]:
+                print("{}: {:d}".format(key, status[key]))

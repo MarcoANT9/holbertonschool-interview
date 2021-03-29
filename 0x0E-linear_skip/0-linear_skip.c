0b8f7b8572c8726d37ca4ab4870c0b5264c7f6e6
MarcoANT9 <acalde27@hotmail.com>
2021-03-29 20:51:49 +0000

    Task 0: First Commit

---
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -0,0 +1,55 @@
+#include "search.h"
+
+/**
+ * linear_skip - Searches for an element in a skip list.
+ *
+ * @list: Pointer to the start of a sorted skip list.
+ * @value: Value to search in the sorted skip list.
+ *
+ *
+ * Description - This program takes a sorted skip linked list and
+ *               searches for a given value, to save time and processing
+ *               capacity, the linked list has "express lanes" to further
+ *               elements within it.
+ *
+ * Return: -> The node of the link that contains the searched value on success.
+ *         -> NULL if the linked list is NULL or doesn't contain the searched
+ *            value.
+ *
+ */
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+	skiplist_t *tmp = list, *save;
+
+	if (list == NULL)
+		return (NULL);
+
+	save = tmp->express;
+	while (save)
+	{
+		printf("Value checked at index [%lu] = [%d]\n",
+		       save->index, save->n);
+
+		if (save->n >= value)
+			break;
+		tmp = save;
+		if (save->express == NULL)
+		{
+			while (save->next)
+				save = save->next;
+			break;
+		}
+		save = save->express;
+	}
+	printf("Value found between indexes [%lu] and [%lu]\n",
+	       tmp->index, save->index);
+	while (tmp != save)
+	{
+		printf("Value checked at index [%lu] = [%d]\n", tmp->index,
+		       tmp->n);
+		if (tmp->n == value)
+			return (tmp);
+		tmp = tmp->next;
+	}
+	return (NULL);
+}

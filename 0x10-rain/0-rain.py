0d43b39bb3100850e8aa422e5123f9053d715135
MarcoANT9 <acalde27@hotmail.com>
2021-04-14 20:41:38 +0000

    Task: Updated logic

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -13,36 +13,18 @@ of width 1 and walls[i] height.
 def rain(walls):
     """This returns the amount of stored water or 0 if the list is empty."""
 
-    wallsLeng = len(walls)
-
-    if (wallsLeng < 1):
+    if len(walls) < 1:
         return 0
 
-    totalRain = 0
-    i = 0
+    rainWater = 0
 
-    while (i < wallsLeng):
-        if ((i - 1) < 0 or i + 1 == wallsLeng):
-            i += 1
-            continue
-        if (walls[i] == 0):
-            zeroes = 1
-            prevWall = walls[i - 1]
-            nextWall = walls[i + 1]
 
-            if (nextWall == 0):
-                while (nextWall == 0):
-                    i += 1
-                    if (i == wallsLeng):
-                        zeroes += 1
-                        break
-                    nextWall = walls[i]
-                    zeroes += 1
-                zeroes -= 1
-
-            heigh = min(prevWall, nextWall)
-            totalRain += heigh * zeroes
-
-        i += 1
-
-    return (totalRain)
+    for element in range(len(walls)):
+        first = walls[element]
+        for wal in range(element):
+            first = max(first, walls[wal])
+        last = walls[element]
+        for wal in range(element + 1, len(walls)):
+            last = max(last, walls[wal])
+        rainWater += (min(first, last) - walls[element])
+    return rainWater

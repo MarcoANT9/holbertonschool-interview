efe61128720a414ee4fbd63c384d1543e413bc4e
MarcoAnt9 <acalde27@hotmail.com>
2021-05-27 14:58:53 -0500

    Task 0: Pep8 Revision

--- 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -3,7 +3,12 @@
 """
 Rotate a 2D Matrix 90°
 """
+
+
 def rotate_2d_matrix(matrix):
+    """
+    Function to rotate matrix
+    """
     tmp_mat = matrix.copy()
 
     for i in range(len(matrix[0])):
@@ -11,4 +16,3 @@ def rotate_2d_matrix(matrix):
         for row in reversed(tmp_mat):
             tmp.append(row[i])
         matrix[i] = tmp
-

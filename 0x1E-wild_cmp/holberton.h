2ac17a3b512a6a909af4586bbb697201c8ed67b6
MarcoANT9 <acalde27@hotmail.com>
2021-07-26 20:46:31 +0000

    Task 0: Upload and header revision

--- 0x1E-wild_cmp/holberton.h
+++ 0x1E-wild_cmp/holberton.h
@@ -5,51 +5,19 @@
 /**-LIBRARIES--------------------------------------------------------------*/
 /**========================================================================*/
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <limits.h>
 
 
 /**========================================================================*/
 /**-STRUCTURES & DEFINITIONS-----------------------------------------------*/
 /**========================================================================*/
 
-/**
- * struct binary_tree_s - Binary tree node
- *
- * @n: Integer stored in the node
- * @parent: Pointer to the parent node
- * @left: Pointer to the left child node
- * @right: Pointer to the right child node
- */
-struct binary_tree_s
-{
-	int n;
-	struct binary_tree_s *parent;
-	struct binary_tree_s *left;
-	struct binary_tree_s *right;
-};
-
-typedef struct binary_tree_s binary_tree_t;
-
-/** AVL Tree */
-typedef struct binary_tree_s avl_t;
-
 
 /**========================================================================*/
 /**-FUNCTIONS--------------------------------------------------------------*/
 /**========================================================================*/
 
-/** 0.Is AVL */
-int binary_tree_is_avl(const binary_tree_t *tree);
-
-/** A. Print AVL Tree */
-int print_t(const binary_tree_t *tree, int offset, int depth, char **s);
-
-/** B. Create a node for the tree */
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-
-void binary_tree_print(const binary_tree_t *tree);
+/** 0. Wild Search */
+int wildcmp(char *s1, char *s2);
 
 /**========================================================================*/
 /**-SPECIAL NOTES & OTHERS-------------------------------------------------*/
